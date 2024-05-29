d909b282352067565dd3eb36d22df1cddb343f15
LaptopHuyThanhHoa <nguyenhoan.world@gmail.com>
2024-05-29 21:50:07 +0700

    first commit

---
+++ src/main/java/com/example/demo/repositories/LopHocRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repositories;
+
+import com.example.demo.entities.LopHoc;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LopHocRepository extends JpaRepository<LopHoc, Long> {
+}
